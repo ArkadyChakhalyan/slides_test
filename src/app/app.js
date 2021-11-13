bb0cb880b02a28868c18d3be58933370c9f529c1
Arkady Chakhalyan <arkadychakhalyan@gmail.com>
2021-11-13 09:15:40 +0300

    123

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { Footer } from "../footer/footer";
 import { Header } from "../header/header";
 import { Slider } from "../slider/slider";
 import './app.scss';
@@ -14,7 +13,6 @@ export const App = () => {
       <Slider 
         homePressed={homePressed}
         resetHomePressed={() => setHomePressed(false)} />
-      <Footer />
     </div>
   );
 };
